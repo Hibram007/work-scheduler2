3d6ae8af63552d271e45bfd1a1446ee02babd867
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-09-18 16:46:44 -0700

    HTML updated and styles centered

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-function CalTime() {
-    $(document).ready(function () {
-      var momentVar = moment().format('dddd, MMMM Do, YYYY h:mm a');
-      var times = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
-        "5 p.m."]
+// moment js code - time and date functionality 
+
+window.alert( "this shit b workin");
+
+//moment().format();
+//moment(Date);
 
 
-
-// time block div
-var columndivEl = document.querySelector("col-12");
-
-var timeblockcontainer = document.querySelector("time-block-list");
-
+//var = new Date(2021, 9, 16);
+//var = daywrapper = moment(day)
 
