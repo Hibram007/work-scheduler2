34fd4ad17dad9e312a54d763f9230816a5ba4bb4
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-09-18 16:55:19 -0700

    linked momentjs to display current date

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// moment js code - time and date functionality 
+// Upper portion current Date and Time display
 
-window.alert( "this shit b workin");
-
-//moment().format();
-//moment(Date);
-
-
-//var = new Date(2021, 9, 16);
-//var = daywrapper = moment(day)
-
+// set where moment js data will go - variable
+var currentDate = moment().format('dddd,MMM do YYYY');
+$("#currentDay").html(currentDate);
